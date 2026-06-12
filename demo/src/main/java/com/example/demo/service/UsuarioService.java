dbb4042b377855e4b45a8c93a56bcd0af72fbd00
JuliaMatildeBorges <juliaborgesmatilde@gmail.com>
2026-06-11 21:40:35 -0300

    Fix: corrigido login

--- demo/src/main/java/com/example/demo/service/UsuarioService.java
+++ demo/src/main/java/com/example/demo/service/UsuarioService.java
@@ -1,21 +1,9 @@
 package com.example.demo.service;
 
-import java.time.LocalDateTime;
-import java.util.Random;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cglib.core.Local;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-
 import com.example.demo.dto.UsuarioDTO;
 import com.example.demo.entity.Usuario;
 import com.example.demo.repository.UsuarioRepository;
-
-import jakarta.transaction.Transactional;
 
 @Service
 public class UsuarioService extends BaseService<Usuario, UsuarioDTO> {
